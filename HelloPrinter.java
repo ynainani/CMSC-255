f2d04ff65489d2c4c29b8c277deb7ecfa756a12b
ynainani <46757097+ynainani@users.noreply.github.com>
2019-01-16 12:11:51 -0500

    Add files via upload

---
+++ HelloPrinter.java
@@ -0,0 +1,27 @@
+/*******************************************************************
+* HelloPrinter
+********************************************************************
+* Project 01 - Your Name in Code
+*___________________________________________________________________
+* Yashna Nainani
+* January 16, 2019
+* CMSC 255 - 04
+********************************************************************/
+
+
+public class HelloPrinter {
+	public static void printHeading() {
+		System.out.println("Yashna Nainani");
+		System.out.println("HelloPrinter");
+		System.out.println("Your Name in Code");
+		System.out.println("January 16, 2019");
+		System.out.println("CMSC 255 - 04");
+		System.out.println();
+	}
+
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
+
